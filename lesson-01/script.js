e1e92d482b991b45108b7db45ebf71e5834ff09f
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-07-23 11:22:51 +0530

    add preview pane

--- lesson-01/script.js
+++ lesson-01/script.js
@@ -4,9 +4,16 @@ new Vue({
     // Some data
     data() {
         return {
-            content: 'This is a note.',
+            content: 'This is a note.'
         }
-    }
+    },
+    // Computed properties
+    computed: {
+        notePreview() {
+            // Markdown rendered to HTML
+            return marked(this.content);
+        },
+    },
 });
 
 const html = marked('**Bold** *Italic* [link] (http: //vuejs.org/)');
