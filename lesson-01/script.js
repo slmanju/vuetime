6667f17e804eacc5fd6f2e4beb0e64701d998b57
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-07-23 12:32:42 +0530

    add styles

--- lesson-01/script.js
+++ lesson-01/script.js
@@ -4,7 +4,8 @@ new Vue({
     // Some data
     data() {
         return {
-            content: 'This is a note.'
+            content: 'This is a note.',
+            notes: [],
         }
     },
     // methods
@@ -12,6 +13,20 @@ new Vue({
         saveNote() {
             console.log('saving note:', this.content)
             localStorage.setItem('content', this.content)
+        },
+        // Add a note with some default content and select it
+        addNote() {
+            const time = Date.now();
+            // Default new note
+            const note = {
+                    id: String(time),
+                    title: 'New note ' + (this.notes.length + 1),
+                    content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+                    created: time,
+                    favorite: false,
+                }
+                // Add to the list
+            this.notes.push(note);
         },
     },
     // Computed properties
