15492cc4b022416c79c3bfedbb60c7dbb104f1d8
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-07-31 09:01:39 +0530

    addd navigation menu

--- lesson-02/src/components/AppLayout.vue
+++ lesson-02/src/components/AppLayout.vue
@@ -4,10 +4,20 @@
             <div><img class="img" src="../assets/logo.svg" /></div>
             <div>My shirt shop</div>
         </header>
-        <!-- Menu will be here -->
+        <NavMenu />
         <router-view />
     </div>
 </template>
+
+<script>
+    import NavMenu from './NavMenu.vue'
+
+    export default {
+        components: {
+            NavMenu,
+        }
+    }
+</script>
 
 <style lang="stylus">
     @import '../style/main';
---
+++ lesson-02/src/components/NavMenu.vue
@@ -0,0 +1,6 @@
+<template>
+    <nav class="menu">
+        <router-link :to="{ name: 'home' }">Home</router-link>
+        <router-link :to="{ name: 'faq' }">FAQ</router-link>
+    </nav>
+</template>
