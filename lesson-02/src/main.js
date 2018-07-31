f0dd5df2a91882a43bc7283e414efe7ab715ea90
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-07-31 09:28:12 +0530

    add a plugin

--- lesson-02/src/main.js
+++ lesson-02/src/main.js
@@ -5,6 +5,10 @@ import Vue from 'vue'
 import router from './router'
 import AppLayout from './components/AppLayout.vue'
 
+import VueFetch from './plugins/fetch'
+
+Vue.use(VueFetch)
+
 new Vue({
     el: '#app',
     render: h => h(AppLayout),
