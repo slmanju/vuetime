cc838fe8c3a4b86d6a0bb6aa42ebb8e15c7ae048
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-07-31 11:17:46 +0530

    add mixins to manage remote data

--- lesson-02/src/components/FAQ.vue
+++ lesson-02/src/components/FAQ.vue
@@ -2,38 +2,29 @@
     <main class="faq">
         <h1>Frenquently Asked Questions</h1>
     
-        <div class="error" v-if="error">
+        <div class="error" v-if="hasRemoteErrors">
             Can't load the questions
         </div>
     
         <section class="list">
-            <article v-for="question of questions">
+            <article v-for="question of questionList">
                 <h2 v-html="question.title"></h2>
                 <p v-html="question.content"></p>
             </article>
         </section>
     
-        <Loading v-if="loading" />
+        <Loading v-if="remoteDataBusy" />
     </main>
 </template>
 
 <script>
+    import RemoteData from '../mixins/RemoteData'
+    
     export default {
-        data() {
-            return {
-                questions: [],
-                error: null,
-                loading: false,
-            }
-        },
-        async created() {
-            this.loading = true;
-            try {
-                this.questions = await this.$fetch('questions')
-            } catch (e) {
-                this.error = e
-            }
-            this.loading = false;
-        },
+        mixins: [
+            RemoteData({
+                questionList: 'questions',
+            }),
+        ]
     }
 </script>
---
+++ lesson-02/src/mixins/RemoteData.js
@@ -0,0 +1,46 @@
+export default function(resources) {
+    return {
+        data() {
+            let initData = {
+                remoteDataLoading: 0,
+            };
+            // Initialize data properties
+            initData.remoteErrors = {}
+            for (const key in resources) {
+                initData[key] = null
+                initData.remoteErrors[key] = null
+            }
+            return initData
+        },
+        methods: {
+            async fetchResource(key, url) {
+                this.$data.remoteDataLoading++
+                    // Reset error
+                    this.$data.remoteErrors[key] = null
+                try {
+                    this.$data[key] = await this.$fetch(url)
+                } catch (e) {
+                    // Put error
+                    this.$data.remoteErrors[key] = e
+                }
+                this.$data.remoteDataLoading--
+            },
+        },
+        created() {
+            for (const key in resources) {
+                let url = resources[key]
+                this.fetchResource(key, url)
+            }
+        },
+        computed: {
+            remoteDataBusy() {
+                return this.$data.remoteDataLoading !== 0
+            },
+            hasRemoteErrors() {
+                return Object.keys(this.$data.remoteErrors).some(
+                    key => this.$data.remoteErrors[key]
+                )
+            },
+        },
+    }
+}
