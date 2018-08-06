56e5f57111cdada36ed97f911bf61441d4fe2c37
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-06 12:12:31 +0530

    show quotes in grid

--- lesson-03/src/App.vue
+++ lesson-03/src/App.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="container">
-        Hello world
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
         data: function () {
             return {
                 quotes: [
-                    'just a quote to see something'
+                    'just a quote to see something',
+                    'hello quote'
                 ],
                 maxQuotes: 10
             }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
         }
     }
 </script>
