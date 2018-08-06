05390aaabbc77d4a7a69b1d0c0d9f3660f53deda
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-06 11:30:14 +0530

    add initial vue components

--- lesson-03/src/App.vue
+++ lesson-03/src/App.vue
@@ -6,7 +6,14 @@
 
 <script>
     export default {
-        
+        data: function () {
+            return {
+                quotes: [
+                    'just a quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        }
     }
 </script>
 
