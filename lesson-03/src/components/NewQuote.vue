17561cbbd2fbde4835adc3d57841104b45610b37
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-06 12:17:27 +0530

    add new quote component

---
+++ lesson-03/src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <label for="quote">Quote</label>
+                <textarea v-model="quote" name="quote" id="quote" cols="30" rows="3" class="form-control"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <button @click.prevent="createNew" class="btn btn-primary">Add Quote</button>
+            </div>
+        </form>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            createNew() {
+                console.log(this.quote);
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
+
